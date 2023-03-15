06507e03df9e65617d92764679f4072226e01484
alena <nakalyona63@gmail.com>
2023-03-15 22:29:16 +0100

    Add first configuration for core service

---
+++ core-service/src/test/java/com/example/Investigation/InvestigationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Investigation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestigationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
